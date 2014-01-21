e673031ea6e85de18493140dec806e31f3e36a33
monkeypushbutton <monkeypushbutton@googlemail.com>
2014-01-21 22:05:21 +0000

    Default leiningen project setup.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to parliamental
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/parliamental/core_test.clj
@@ -0,0 +1,7 @@
+(ns parliamental.core-test
+  (:require [clojure.test :refer :all]
+            [parliamental.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
